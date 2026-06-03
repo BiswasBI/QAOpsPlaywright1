bd322ee82dc8945c4fdbbbace865954ec4aa3de2
arpita <arpitabkeya25@gmail.com>
2026-06-03 21:32:12 +0200

    via AWS ECR; ECS playwright test added - terraform scripts, yml file, updated ecs.tf, iam.tf, s3.tf

---
+++ terraform/s3.tf
@@ -0,0 +1,27 @@
+resource "aws_iam_policy" "playwright_s3" {
+  name = "${var.project_name}-s3-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [{
+      Effect = "Allow"
+
+      Action = [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:ListBucket"
+      ]
+
+      Resource = [
+        aws_s3_bucket.playwright_reports.arn,
+        "${aws_s3_bucket.playwright_reports.arn}/*"
+      ]
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "playwright_s3" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.playwright_s3.arn
+}
