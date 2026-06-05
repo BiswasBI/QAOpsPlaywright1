b82aef49dc03ad46eecd63f605773f248beca335
arpita <arpitabkeya25@gmail.com>
2026-06-05 17:28:10 +0200

    Make changes on dockerfile

--- upload-report.sh
+++ upload-report.sh
@@ -7,7 +7,6 @@ npx playwright test --grep @web
 
 echo "Uploading report to S3..."
 
-aws s3 cp -r playwright-report \
-s3://my-playwright-reports-bucket-114403655647/$(date +%s)/
+aws s3 cp playwright-report s3://my-bucket/reports/ --recursive
 
 echo "Upload complete"
