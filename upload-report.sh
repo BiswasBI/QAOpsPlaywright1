0b56c89a8c206acdc44e55522dc50c7d075e8ba5
arpita <arpitabkeya25@gmail.com>
2026-06-08 10:27:04 +0200

    run from package.json

--- upload-report.sh
+++ upload-report.sh
@@ -3,8 +3,8 @@
 set -e
 
 echo "Running Playwright tests..."
-npx playwright test --grep @web --shard=$SHARD
-
+#npx playwright test --grep @web --shard=$SHARD
+npm run regression
 echo "Uploading report to S3..."
 
 aws s3 cp playwright-report s3://my-playwright-reports-bucket-114403655647/reports/ --recursive
